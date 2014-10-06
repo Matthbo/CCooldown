ae987aa77c22f9406c5cf219d53b9615f2c3b0d2
Matthijs <matthijs.booman@hotmail.com>
2014-10-06 19:03:25 +0200

    config updates when needed!
    old configs are saved in "config_old.yml"

--- src/matthbo/plugin/chatcooldown/ChatCooldown.java
+++ src/matthbo/plugin/chatcooldown/ChatCooldown.java
@@ -22,7 +22,7 @@ public class ChatCooldown extends JavaPlugin{
 	public void onEnable(){
 		instance = this;
 		
-		config.preInitCfg();
+		config.InitCfg();
 		
 		getLogger().info("ChatCooldown has been Activated");
 	}
@@ -33,27 +33,49 @@ public class ChatCooldown extends JavaPlugin{
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(cmd.getName().equalsIgnoreCase("ccooldown")){
-			if(args.length == 1){
-				if(args[0].equalsIgnoreCase("disable")){
-					if(!config.isOn){sender.sendMessage(pluginMSG + "ChatCooldown is already Disabled!");}
+			if(args.length > 0){
+				if(args.length == 1 && args[0].equalsIgnoreCase("disable")){
+					if(!config.isOn){sender.sendMessage(pluginMSG + "ChatCooldown is already Disabled!"); return true;}
 					config.isOn = false;
 					config.set(config.isOn_LANG, false);
 					sender.sendMessage(pluginMSG + "ChatCooldown is Disabled!");
 					
 					return true;
 				}
-				else if(args[0].equalsIgnoreCase("enable")){
+				else if(args.length == 1 && args[0].equalsIgnoreCase("enable")){
 					if(config.isOn){sender.sendMessage(pluginMSG + "ChatCooldown is already Enabled!"); return true;}
 					config.isOn = true;
 					config.set(config.isOn_LANG, true);
 					sender.sendMessage(pluginMSG + "ChatCooldown is Enabled!");
 					return true;
 				}
-				else if(args[0].equalsIgnoreCase("reload")){
+				else if(args.length == 1 && args[0].equalsIgnoreCase("reload")){
 					config.reload();
+					sender.sendMessage(pluginMSG + "Reloaded!");
 					return true;
 				}
-			}else{
+				else if(args[0].equalsIgnoreCase("auto")){
+					if(args.length == 2 && args[1].equalsIgnoreCase("on")){
+						config.autoOn = true;
+						config.set(config.autoOn_LANG, true);
+						sender.sendMessage(pluginMSG + "AutoOn is On!");
+						return true;
+					}
+					if(args.length == 2 && args[1].equalsIgnoreCase("off")){
+						config.autoOn = false;
+						config.set(config.autoOn_LANG, false);
+						sender.sendMessage(pluginMSG + "AutoOn is Off!");
+						return true;
+					}
+					else{
+						if(config.autoOn) sender.sendMessage(pluginMSG + "AutoOn is On!");
+						if(!config.autoOn) sender.sendMessage(pluginMSG + "AutoOn is Off!");
+						return true;
+					}
+				}
+			}
+			
+			else{
 				if(config.isOn){sender.sendMessage(pluginMSG + "ChatCooldown is Enabled!"); return true;}
 				if(!config.isOn){sender.sendMessage(pluginMSG + "ChatCooldown is Disabled!"); return true;}
 				else sender.sendMessage(pluginUsage + "something went wrong! ;(");
--- src/matthbo/plugin/chatcooldown/Config.java
+++ src/matthbo/plugin/chatcooldown/Config.java
@@ -1,23 +1,46 @@
 package matthbo.plugin.chatcooldown;
 
+import java.io.File;
+
 public class Config {
 	
 	public ChatCooldown cc = ChatCooldown.instance;
 	
 	public boolean isOn = true;
+	public boolean autoOn = false;
+	
+	private String cfgVersion = "0";
+	private String cfgNeededVersion = "0.1";
 	
 	public String isOn_LANG = "general.isOn";
+	public String autoOn_LANG = "general.autoOn";
 	
-	public void preInitCfg(){
+	private String cfgVersion_LANG = "doNotChange.cfgVersion";
+	
+	public void InitCfg(){
 		cc = ChatCooldown.instance;
 		cc.saveDefaultConfig();
 		
-		InitCfg();
-	}
-	
-	public void InitCfg(){
+		cfgVersion = cc.getConfig().getString(cfgVersion_LANG);
+		
+		//checks if config is uptodate
+		if(cfgVersion != cfgNeededVersion){
+			try{
+			File dataFolder = cc.getDataFolder();
+			File cfgFile = new File(dataFolder, "config.yml");
+			File oldCfgFile = new File(dataFolder, "config_old.yml");
+			if(oldCfgFile.exists()) oldCfgFile.delete();
+			cfgFile.renameTo(oldCfgFile);
+			cc.saveDefaultConfig();
+			cc.getLogger().warning("New Config File Created!");
+			cc.getLogger().warning("Old Configs Are Saved In 'config_old.yml'!");
+			}catch(Exception e) {e.printStackTrace();}
+		}
+		
 		//general
 		isOn = cc.getConfig().getBoolean(isOn_LANG);
+		autoOn = cc.getConfig().getBoolean(autoOn_LANG);
+		
 	}
 	
 	public void reload(){
